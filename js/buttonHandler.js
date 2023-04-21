c20ae1891adb9e94cb468e92bf737c0072054513
Junhyeok2004Dec <cjhbest1209@gmail.com>
2023-04-21 22:16:54 +0900

    update: buttonHandler

--- js/buttonHandler.js
+++ js/buttonHandler.js
@@ -1,15 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     var insertBtn = document.getElementById("input");
-    var resultComponent = document.getElementById("results");
 
 
-    const text = document.getElementById
-    ('textInput').value; // 값 불러오기 안 됨
-
-    console.log(text);
     insertBtn.addEventListener("click", function() {
-        resultComponent.innerText = text;
-
+        var resultComponent = document.getElementById("results");
+        
+        const text = document.getElementById
+        ('textInput').value; // 값 불러오기 안 됨
+            resultComponent.innerText = text;
     });
-})
+});

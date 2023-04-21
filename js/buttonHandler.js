442e240f19e9e407b74e4d384c21a0586b4eed5c
Junhyeok2004Dec <cjhbest1209@gmail.com>
2023-04-21 22:57:17 +0900

    add: js/reload.js update: index.html, buttonHandler.js, main.js, time.js 매 순간마다(1초 간격 정도) 시간 등 update 하기 위해 reload.js를 설계하였음. main과 통합될 수 있음. chrome reload 사용예정

--- js/buttonHandler.js
+++ js/buttonHandler.js
@@ -2,12 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var insertBtn = document.getElementById("input");
 
-
     insertBtn.addEventListener("click", function() {
         var resultComponent = document.getElementById("results");
         
         const text = document.getElementById
         ('textInput').value; // 값 불러오기 안 됨
-            resultComponent.innerText = text;
+            resultComponent.innerText = ":: 공지 :: \n" + text;
     });
+
+
+    
+
+    
 });
